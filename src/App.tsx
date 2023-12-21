04228d4552a3cbd3276133fb5a0de0354a381942
yibinllin <linyibin1997@126.com>
2023-12-22 00:04:50 +0800

    feat: list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import BeautifulTabs from "./BeautifulTabs/BeautifulTabs";
+
 function App() {
-  return <></>;
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <BeautifulTabs 
+        list={['tab1', 'tab2', 'tab3']}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/BeautifulTabs/BeautifulTabs.tsx
+++ src/BeautifulTabs/BeautifulTabs.tsx
@@ -1,7 +1,11 @@
+import './BeautifulTabs.css'
+
 interface BeautifulTabsProps<T> {
   list: T[];
 }
 
-function BeautifulTabs<T>({ list }: BeautifulTabsProps<T>) {}
+function BeautifulTabs<T>({ list }: BeautifulTabsProps<T>) {
+  return <div className="tabs"></div>;
+}
 
 export default BeautifulTabs;
